aa21b68fef3a4b6f516368dd06d771a8015bf2fc
Lab3 <hamsonolotu@cmail.carleton.ca>
2025-09-26 16:08:40 -0400

    First commit

---
+++ src/BuddyInfo.java
@@ -0,0 +1,23 @@
+public class BuddyInfo {
+    private String name;
+    private String address;
+    private String phone;
+
+    public BuddyInfo(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public BuddyInfo() {
+        this("Joe", "123 Street", "123456789");
+    }
+
+    public String getName() {
+        return name;
+    }
+    public static void main(String[] args) {
+        BuddyInfo bud = new BuddyInfo("Homer", "123 Street", "123456789");
+        System.out.println("Hello " + bud.getName());
+    }
+}
